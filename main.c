8bd3af1ba1f32f9208c18b4491e2c7fcc0ba8996
Your Name <you@example.com>
2022-04-23 17:44:40 -0400

    tipo Producto creado

--- main.c
+++ main.c
@@ -4,3 +4,10 @@
 #include "hashmap.h"
 #include "list.h"
 
+typedef struct {
+    char nombre[32];
+    char marca[16];
+    char tipo[16];
+    unsigned int stock;
+    unsigned int precio;
+} Producto;
